32cbd1dba2d8050dfef8668ffe8be5997c0c7745
MyGitcode <MyGitcode@qq.com>
2026-01-21 09:56:05 +0800

    -

--- src/niceguiEg/src/02/checkboxEg.py
+++
@@ -1,7 +0,0 @@
-from nicegui import ui
-
-agree = ui.checkbox('我同意用户协议')
-ui.label().bind_text_from(agree, 'value', 
-                         backward=lambda v: f'协议状态: {"已同意" if v else "未同意"}')
-
-ui.run()
